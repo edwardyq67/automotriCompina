256967eaca4e71a7f74b6528e91323607268b24c
edwardyq67 <edwardyq67@gmail.com>
2024-09-04 06:26:00 -0500

    casi listo6

--- src/NavBarFooter/NavBar.jsx
+++ src/NavBarFooter/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { GiHamburgerMenu } from 'react-icons/gi'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { AccordionItem } from './AccordionItem'; // Asegúrate de que la ruta sea correcta
 import { MdClose } from 'react-icons/md';
 import { Accordionpc } from './Accordeonpc';
@@ -8,7 +8,7 @@ import { Accordionpc } from './Accordeonpc';
 export default function NavBar() {
     const [open, setOpen] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
-    const navigate = useNavigate();
+    const location = useLocation();
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 0) {
@@ -21,6 +21,12 @@ export default function NavBar() {
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
+    useEffect(() => {
+        // Cerrar el menú cuando la ubicación cambie
+        if (open) {
+            setOpen(false);
+        }
+    }, [location]);
     const NavBarMenu = [
         {
             id: 1,
@@ -52,22 +58,28 @@ export default function NavBar() {
 
     return (
         <nav
-            className={`fixed py-5 z-20 w-[100vw] border-b-[.1px] border-gray-800 transition-colors duration-300 ${isScrolled ? 'bg-black' : 'bg-transparent'
-                }`}
+            className={`fixed py-5 z-20 w-full border-b-[0.1px] border-gray-800 transition-colors duration-300 ${isScrolled ? 'bg-black' : 'bg-transparent'}`}
         >
-            <div className='flex bg-red-500 max-w-[90vw] mx-auto justify-between items-center relative'>
-                <img className='h-[35px]' src="https://res.cloudinary.com/dcwdddwnh/image/upload/v1725303756/images_app/logoAutomotriz-removebg-preview-transformed.webp" alt="" />
-                <ul className='hidden gap-4 justify-between md:flex'>
+            <div className="flex max-w-[90vw] mx-auto justify-between items-center relative">
+                <img
+                    className="h-[35px]"
+                    src="https://res.cloudinary.com/dcwdddwnh/image/upload/v1725303756/images_app/logoAutomotriz-removebg-preview-transformed.webp"
+                    alt="Logo"
+                />
+                <ul className="hidden gap-4 md:flex">
                     {NavBarMenu.map(menu => (
                         <li
                             key={menu.id}
-                            className='font-poppins uppercase font-medium text-base text-gray-400  focus:text-white hover:text-white active:text-white transition-all duration-200 hover:shadow-[0_3px_0_-1px_#ffffff] px-2'
+                            className="font-poppins uppercase font-medium text-base text-gray-400 focus:text-white hover:text-white active:text-white transition-all duration-200 hover:shadow-[0_3px_0_-1px_#ffffff] px-2"
                         >
                             {menu.submenu ? (
                                 <Accordionpc title={menu.titulo}>
-                                    <ul className="">
+                                    <ul className="pl-4">
                                         {menu.submenu.map((sub, idx) => (
-                                            <li key={idx} className="cursor-pointer text-nowrap text-gray-400 hover:text-white transition-all duration-200 text-base py-1">
+                                            <li
+                                                key={idx}
+                                                className="cursor-pointer text-gray-400 hover:text-white transition-all duration-200 text-base py-1"
+                                            >
                                                 <Link to={sub.Link}>{sub.titulo}</Link>
                                             </li>
                                         ))}
@@ -80,20 +92,19 @@ export default function NavBar() {
                     ))}
                 </ul>
                 <div className="md:hidden flex" onClick={() => setOpen(!open)}>
-                    <GiHamburgerMenu color='white' size={24} />
+                    <GiHamburgerMenu color="white" size={24} />
                 </div>
                 <div
-                    className={`fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform ${open ? 'translate-x-0' : 'translate-x-full'
-                        } bg-white w-80 dark:bg-secondary-950`}
+                    className={`fixed top-0 right-0 z-40 h-screen p-4 overflow-y-auto transition-transform transform ${open ? 'translate-x-0' : 'translate-x-full'} bg-white w-80 dark:bg-secondary-950`}
                     tabIndex="-1"
                     aria-labelledby="drawer-right-label"
                 >
-                    <div className="flex justify-end" >
-                        <MdClose color='white' size={30} onClick={() => setOpen(!open)} />
+                    <div className="flex justify-end">
+                        <MdClose color="white" size={30} onClick={() => setOpen(!open)} />
                     </div>
-                    <ul className='font-semibold uppercase  text-black py-5  rounded-lg'>
+                    <ul className="font-semibold uppercase text-black py-5 rounded-lg">
                         {NavBarMenu.map(menu => (
-                            <li key={menu.id} className='mb-4 text-2xl text-white'>
+                            <li key={menu.id} className="mb-4 text-2xl text-white">
                                 {menu.submenu ? (
                                     <AccordionItem title={menu.titulo}>
                                         <ul className="pl-4 grid gap-2">
@@ -113,5 +124,6 @@ export default function NavBar() {
                 </div>
             </div>
         </nav>
+
     )
 }
